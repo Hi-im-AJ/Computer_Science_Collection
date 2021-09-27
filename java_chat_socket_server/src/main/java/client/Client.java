a622a5458fb89fc39d08bbef4bbec91eea3d2634
Anders Højer Jørgensen <virtualzeth@gmail.com>
2021-09-27 11:53:22 +0200

    Fixed unset autoflush param

--- java_chat_socket_server/src/main/java/client/Client.java
+++ java_chat_socket_server/src/main/java/client/Client.java
@@ -7,32 +7,24 @@ import java.util.Scanner;
 public class Client {
     static final String SERVER_IP = "127.0.0.1";
     static final Integer SERVER_PORT = 5000;
+
+    static BufferedReader input;
+    static PrintWriter output;
     static Scanner scanner = new Scanner(System.in);
     static String name;
 
     public static void main(String[] args) {
-            try(Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+        try(Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
 
-            System.out.print("Please provide your username: ");
-            while (true) {
-                name = scanner.nextLine();
-                output.write(name);
-                if(input.readLine().equals("FREE")) {
-                    break;
-                } else {
-                    System.out.println("That name is already taken");
-                }
-            }
+            provideUsername();
 
-            System.out.printf("Welcome %s%n", name);
-            System.out.println("You can now start chatting!");
             while(true) {
                 System.out.printf("%s: ", name);
                 String msg = scanner.nextLine();
                 output.println(msg);
-                if(msg.equals("exit")) break;
+                if(msg.equals("!exit")) break;
                 System.out.println(input.readLine());
             }
             input.close();
@@ -40,5 +32,22 @@ public class Client {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void provideUsername() throws IOException {
+        System.out.print("Please provide your username: ");
+        while (true) {
+            name = scanner.nextLine();
+            output.println(name);
+            String response = input.readLine();
+            if(response.equals("FREE")) {
+                break;
+            } else {
+                System.out.println("That name is already taken");
+            }
+        }
+
+        System.out.printf("Welcome %s%n", name);
+        System.out.println("You can now start chatting!");
     }
 }
--- java_chat_socket_server/src/main/java/server/ClientThread.java
+++ java_chat_socket_server/src/main/java/server/ClientThread.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -11,36 +12,47 @@ public class ClientThread extends Thread {
     public LinkedList<String> usernames;
 
     private final Socket SOCKET;
+    private final BufferedReader input;
+    private final PrintWriter output;
 
-    public ClientThread(Socket socket, LinkedList<String> usernames) {
+    public ClientThread(Socket socket, LinkedList<String> usernames) throws IOException {
         this.SOCKET = socket;
         this.usernames = usernames;
+        this.input = new BufferedReader(new InputStreamReader(SOCKET.getInputStream()));
+        this.output = new PrintWriter(SOCKET.getOutputStream(), true);
     }
 
     @Override
     public void run() {
         try {
-            BufferedReader input = new BufferedReader(new InputStreamReader(SOCKET.getInputStream()));
-            PrintWriter output = new PrintWriter(SOCKET.getOutputStream());
-            
-            AtomicBoolean nameTaken = new AtomicBoolean(false);
-            while(true) {
-                String username = input.readLine();
-                usernames.forEach((e) -> {
-                    if(username.equals(e)) nameTaken.set(true);
-                });
-                if(nameTaken.get()) {
-                    output.println("TAKEN");
-                    break;
-                }
-            }
-            output.println("FREE");
+            boolean gate = validateUsername();
 
-            while(true) {
+            while(gate) {
                 output.println("Test message");
                 Thread.sleep(5000);
             }
-            
-        } catch (Exception ignored) {}
+            SOCKET.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean validateUsername() throws IOException {
+        AtomicBoolean nameTaken = new AtomicBoolean(false);
+        String username;
+        while(true) {
+            username = input.readLine();
+            String finalUsername = username;
+            usernames.forEach((e) -> {
+                if(finalUsername.equals(e)) nameTaken.set(true);
+            });
+            if(nameTaken.get()) {
+                output.println("TAKEN");
+            } else {
+                output.println("FREE");
+                usernames.add(username);
+                return true;
+            }
+        }
     }
 }
