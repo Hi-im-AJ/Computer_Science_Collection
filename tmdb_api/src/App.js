45988e2212dc9d0128d6da306b521e304dec038d
Anders Højer Jørgensen <virtualzeth@gmail.com>
2021-10-12 21:10:13 +0200

    TMDB: add movie collection styling

--- tmdb_api/src/App.js
+++ tmdb_api/src/App.js
@@ -1,41 +1,54 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Form, Button, Row, Col, Container } from "react-bootstrap";
+import Movie from "./components/Movie";
 
 function App() {
-  const [searchList, setSearchList] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [searchResult, setSearchResult] = useState(null);
+  const [lastQuery, setLastQuery] = useState("");
   const [input, setInput] = useState("");
 
-  const search = async (query) => {
-    console.log(query);
+  const search = async (query, page = 1) => {
+    setLastQuery(query);
     const res = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&query=${lastQuery}&page=${page}&include_adult=false`
     );
-    setSearchList(res.data);
+    setSearchResult(res.data);
+    page !== 1 ? setMovies([...movies, res.data.results]) : setMovies([]);
   };
-  const onInputChange = (e) => setInput(e.target.value);
+  const onChange = (e) => setInput(e.target.value);
+  const onKeyPress = (e) => (e.code === "Enter" ? search(input) : undefined);
 
   return (
     <div className="App">
-      <Form as={Row} className="m-3">
-        <Form.Group as={Col}>
-          <Form.Control onChange={(e) => onInputChange(e)} type="text" placeholder="Search" />
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Button onClick={() => search(input)} variant="primary" type="submit">
-            Search
-          </Button>
-        </Form.Group>
-      </Form>
-      <div>
-        <table>
-          {searchList.results.map((e) => (
-            <tr>
-              <td>{e.title}</td>
-            </tr>
-          ))}
-        </table>
-      </div>
+      <Container>
+        <Form as={Row} className="m-3">
+          <Form.Group sm={10} as={Col}>
+            <Form.Control
+              onKeyPress={(e) => onKeyPress(e)}
+              onChange={(e) => onChange(e)}
+              type="text"
+              placeholder="Search"
+            />
+          </Form.Group>
+          <Form.Group sm={2} as={Col}>
+            <Button onClick={() => search(input)} variant="primary" type="submit">
+              Search
+            </Button>
+          </Form.Group>
+        </Form>
+      </Container>
+      <Container className="">
+        <Row>
+          {searchResult != null &&
+            searchResult.results.map((e) => (
+              <Col key={e.id} className="m-3">
+                <Movie movie={e} />
+              </Col>
+            ))}
+        </Row>
+      </Container>
     </div>
   );
 }
