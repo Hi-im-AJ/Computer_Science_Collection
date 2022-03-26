4f198053146776d96e27b1bb857a65419fc65755
Anders Jørgensen <38241145+hi-im-aj@users.noreply.github.com>
2022-03-26 16:40:56 +0100

    Add navigation

--- react_redux_setup_template/src/App.tsx
+++ react_redux_setup_template/src/App.tsx
@@ -1,13 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Popular from "./components/Popular";
+import About from "./components/About";
+import Movie from "./components/Movie";
 
 const App = () => {
   return (
     <main>
-      <Navbar />
       <BrowserRouter>
-        <Popular />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Popular />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/movie" element={<Movie />} />
+        </Routes>
       </BrowserRouter>
     </main>
   );
--- react_redux_setup_template/src/components/Navbar.tsx
+++ react_redux_setup_template/src/components/Navbar.tsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav>
-      <input placeholder="search" />
-      <button>Search</button>
+      <ul style={{ listStyle: "none" }}>
+        <li>
+          <input placeholder="search" />
+          <button>Search</button>
+        </li>
+        <li style={{ margin: "1rem 0" }}>
+          <Link to="/">Home</Link>
+          <Link style={{ margin: "0 1rem" }} to="/about">
+            About
+          </Link>
+          <Link to="/movie">Movie</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
