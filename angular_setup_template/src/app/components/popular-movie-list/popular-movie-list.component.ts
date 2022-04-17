04724dc51958fab6d22c047ec38417f67e1f664f
Anders Jørgensen <38241145+hi-im-aj@users.noreply.github.com>
2022-04-17 23:00:17 +0200

    Initial commit angular setup template

---
+++ angular_setup_template/src/app/components/popular-movie-list/popular-movie-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-movie-list',
+  templateUrl: './popular-movie-list.component.html',
+  styleUrls: ['./popular-movie-list.component.scss']
+})
+export class PopularMovieListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
