0021e4f1ad58e1c2ae1774aa33add63bd7356261
Anders Jørgensen <38241145+hi-im-aj@users.noreply.github.com>
2022-04-19 17:45:17 +0200

    Angular template app: update routing

--- angular_setup_template/src/app/app.module.ts
+++ angular_setup_template/src/app/app.module.ts
@@ -2,31 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PopularMovieListComponent } from './components/popular-movie-list/popular-movie-list.component';
-import { MovieComponent } from './components/movie/movie.component';
-import { AboutComponent } from './components/views/about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    PopularMovieListComponent,
-    MovieComponent,
-    AboutComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', component: PopularMovieListComponent },
-      { path: 'about', component: AboutComponent },
-    ]),
-  ],
+  declarations: [AppComponent, NavbarComponent, PopularMovieListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
